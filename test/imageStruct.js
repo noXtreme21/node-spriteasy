320990b774627f6ada8b0621df8d67fe082b37de
noXtreme21 <cg@zoundfx.com>
2012-05-20 04:37:06 +0200

    Add new structure for spriteasy (very experimental)

---
+++ test/imageStruct.js
@@ -0,0 +1,158 @@
+var ImageStruct = require('../lib/index')
+  , Canvas      = require('canvas')
+  , Image       = Canvas.Image
+  , should      = require('should');
+
+var imageStruct = new SPRITEASY.ImageStruct(
+    new SPRITEASY.ImageReader({
+        image: (function() {
+            var image = new Image();
+            image.src = 'test_image.png';
+            return image;
+        })()
+      , width: 100
+      , height: 100
+      , totalWidth: 200
+      , totalHeight: 200
+    })
+);
+
+describe('ImageStruct', function(){
+    describe('getImage', function(){
+        it('should return an image object', function(){
+        var image = imageStruct.getImage();
+        image.should.be.a('object');
+        image.src.should.be.equal('test_image.png');
+    });
+  })
+});
+
+
+describe('ImageStruct', function(){
+    describe('getWidth', function(){
+        it('should return the width of the image', function(){
+        var width = imageStruct.getWidth();
+        width.should.be.a('number');
+        width.should.be.equal(100);
+    });
+  })
+});
+
+
+describe('ImageStruct', function(){
+    describe('getHeight', function(){
+        it('should return the height of the image', function(){
+        var height = imageStruct.getHeight();
+        height.should.be.a('number');
+        height.should.be.equal(100);
+    });
+  })
+});
+
+
+describe('ImageStruct', function(){
+    describe('getTotalWidth', function(){
+        it('should return the total width of the image', function(){
+        var width = imageStruct.getTotalWidth();
+        width.should.be.a('number');
+        width.should.be.equal(200);
+    });
+  })
+});
+
+
+describe('ImageStruct', function(){
+    describe('getTotalHeight', function(){
+        it('should return the total height of the image', function(){
+        var height = imageStruct.getTotalHeight();
+        height.should.be.a('number');
+        height.should.be.equal(200);
+    });
+  })
+});
+
+
+describe('ImageStruct', function(){
+    describe('getLeft', function(){
+        it('should return the left position of the image', function(){
+        var left = imageStruct.getLeft();
+        left.should.be.a('number');
+        left.should.be.equal(100);
+    });
+  })
+});
+
+
+describe('ImageStruct', function(){
+    describe('getTop', function(){
+        it('should return the top position of the image', function(){
+        var top = imageStruct.getTop();
+        top.should.be.a('number');
+        top.should.be.equal(100);
+    });
+  })
+});
+
+
+describe('ImageStruct', function(){
+    describe('setImage', function(){
+        it('should change the image object', function(){
+        imageStruct.setImage((function() {
+            var image = new Image();
+            image.src = 'test_image_changed.png';
+            return image;
+        })());
+        var image = imageStruct.getImage();
+        image.should.be.a('object');
+        image.src.should.be.equal('test_image_changed.png');
+    });
+  })
+});
+
+
+describe('ImageStruct', function(){
+    describe('setWidth', function(){
+        it('should change the width of the image', function(){
+        imageStruct.setWidth(200);
+        var width = imageStruct.getWidth();
+        width.should.be.a('number');
+        width.should.be.equal(200);
+    });
+  })
+});
+
+
+describe('ImageStruct', function(){
+    describe('setHeight', function(){
+        it('should change the height of the image', function(){
+        imageStruct.setHeight(200)
+        var height = imageStruct.getHeight();
+        height.should.be.a('number');
+        height.should.be.equal(200);
+    });
+  })
+});
+
+
+describe('ImageStruct', function(){
+    describe('setTotalWidth', function(){
+        it('should change the total width of the image', function(){
+        imageStruct.setTotalWidth(400)
+        var width = imageStruct.getTotalWidth();
+        width.should.be.a('number');
+        width.should.be.equal(400);
+    });
+  })
+});
+
+
+describe('ImageStruct', function(){
+    describe('getTotalHeight', function(){
+        it('should change the total height of the image', function(){
+        imageStruct.setTotalHeight(400)
+        var height = imageStruct.getTotalHeight();
+        height.should.be.a('number');
+        height.should.be.equal(400);
+    });
+  })
+});
